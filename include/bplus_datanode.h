1039f48bf4546349d4e334bd9a6e2d8e5949f267
foivos21 <sdi1900313@di.uoa.gr>
2025-11-29 20:07:10 +0200

    Update bplus_datanode.h

--- include/bplus_datanode.h
+++ include/bplus_datanode.h
@@ -6,15 +6,14 @@
 /* Στο αντίστοιχο αρχείο .h μπορείτε να δηλώσετε τις συναρτήσεις
  * και τις δομές δεδομένων που σχετίζονται με τους Κόμβους Δεδομένων.*/
 
-#endif
+#define BPLUS_MAX_DATA_RECORDS \
+  ((BF_BLOCK_SIZE - 3 * sizeof(int)) / sizeof(Record))
 
-struct dataNode {
-    int branchingFactor; //TODO ideally move to overall tree metadata
-    //metadata, then:
-    struct sll_node* head;
-    struct dataNode* tail;
-};
+typedef struct {
+    int is_leaf;                     
+    int num_records;                 
+    int next_leaf;                  
+    Record records[BPLUS_MAX_DATA_RECORDS];  
+} BPlusDataNode;
 
-int leaf_search(int key, struct dataNode* self);
-int leaf_insert(int key, struct dataNode* self, const Record *record);
-struct dataNode* leaf_split(struct dataNode* self);
+#endif /* BP_DATANODE_H */
